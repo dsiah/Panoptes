db85aa90e4d2e33c57ba9fcd2e44f0678be1fe38
david siah <dsiah@villanova.edu>
2016-01-19 21:33:51 -0500

    init

---
+++ js/test.js
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Title = require('./Components/Title.jsx');
+
+ReactDOM.render(<Title />, document.getElementById('content'));
