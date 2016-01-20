3443d54650f71449e53d35486d76d53ac6c72ef0
david siah <dsiah@villanova.edu>
2016-01-19 22:19:01 -0500

    <3 u al

--- js/Components/CheckIcon.jsx
+++ js/Components/CheckIcon.jsx
@@ -2,6 +2,6 @@ var React = require('react');
 
 module.exports = React.createClass({
 	render: function() {
-		return <div className="medal-icon">&#x2713;</div>;
+		return <span className="medal-icon">&#x2713;</span>;
 	}
 });
